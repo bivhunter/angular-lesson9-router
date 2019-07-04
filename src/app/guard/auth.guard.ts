00607f0e84a976746e5c2a252f000ce87b01107a
bivhunter <bivhunter@ukr.net>
2019-07-04 15:47:12 +0300

    add guard and component register

---
+++ src/app/guard/auth.guard.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from "../services/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {
+
+  }
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log(this.authService.checkAuth().subscribe(auth => auth));
+
+    return new Promise((resolve, reject) => {
+      this.authService.checkAuth().subscribe(auth => {
+          resolve(auth);
+      });
+    })
+      .then((auth) => {
+        if (!auth) {
+          this.router.navigate(['/login']);
+          return false;
+        } else {
+          return true;
+        }
+      })
+      /*.catch(() => {
+        console.log("login");
+        return false;
+      });*/
+
+
+    /*return .map(auth => {
+      console.log(auth);
+      if(!auth) {
+        this.router.navigate(['/login']);
+        return false;
+      } else {
+        return true;
+      }
+    });*/
+  }
+
+}
